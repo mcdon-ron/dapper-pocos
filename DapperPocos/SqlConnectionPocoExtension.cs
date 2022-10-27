a9146b271fc46f824cf18d0b98f63d53904f1d68
mcdon-ron <2132322+mcdon-ron@users.noreply.github.com>
2022-10-27 14:47:29 -0500

    If there's a Sql string type with a length specifier, want to include the length as a comment for the POCO's property. If it's a string type with a length of 1 the the POCO property should be a char instead of a string.

--- DapperPocos/SqlConnectionPocoExtension.cs
+++ DapperPocos/SqlConnectionPocoExtension.cs
@@ -44,10 +44,10 @@ namespace PocoExtension
             foreach (var item in list)
             {
                 string name = item.name;
-                string stype = item.system_type_name;
+                string sqlType = item.system_type_name;
                 bool nullable = item.is_nullable;
 
-                AppendProperty(sb, stype, nullable, name);
+                AppendProperty(sb, sqlType, nullable, name);
             }
             sb.AppendLine("}");
             return sb.ToString();
@@ -55,9 +55,18 @@ namespace PocoExtension
 
         private static void AppendProperty(StringBuilder stringBuilder, string sqlType, bool nullable, string name)
         {
+            sqlType = sqlType.Trim();
+            name = name.Trim();
+
             string csType = null;
-            if (TryMap(sqlType, nullable, out csType))
-                stringBuilder.AppendLine($"    public {csType} {name} {{ get; set; }}");
+            string length = null;
+            if (TryMap(sqlType, nullable, out csType, out length))
+            {
+                if (csType == "string" && !string.IsNullOrEmpty(length))
+                    stringBuilder.AppendLine($"    public {csType} {name} {{ get; set; }} // Length: {length}");
+                else
+                    stringBuilder.AppendLine($"    public {csType} {name} {{ get; set; }}");
+            }
             else
             {
                 stringBuilder.AppendLine($"    // TODO: Add Mapping for Unknown SQL Server Data Type: {sqlType}");
@@ -66,14 +75,36 @@ namespace PocoExtension
             }
         }
 
-        public static bool TryMap(string sqlType, bool nullable, out string csType)
+        private static string[] ParseSqlType(string sqlType)
+        {
+            var result = new string[] {null, null};
+
+            // split the type and any length specifier
+            // like nvarchar(x)
+            var parts = sqlType.Trim(')').Split('(');
+
+            // the type name
+            result[0] = parts[0].Trim().ToLower();
+
+            // if we have a length specifier
+            if (parts.Length > 1)
+            {
+                // the length specifier
+                result[1] = parts[1].Trim();
+            }
+
+            return result;
+        }
+
+        public static bool TryMap(string sqlType, bool nullable, out string csType, out string stringLength)
         {
             // for handling nullable value types
             var n = nullable ? "?" : string.Empty;
 
-            // drop any length specifier
-            // like nvarchar(x) -> nvarchar
-            var type = sqlType.Split('(')[0];
+            var parsedType = ParseSqlType(sqlType);
+            var type = parsedType[0];
+            stringLength = parsedType[1];
+
             // sql types that are commented out
             // are those I don't know how to
             // properly map to clr types
@@ -99,7 +130,10 @@ namespace PocoExtension
                 case "ntext":
                 case "varchar":
                 case "nvarchar":
-                    csType = "string"; // String
+                    if (stringLength == "1")
+                        csType = "char"; // Char
+                    else
+                        csType = "string"; // String
                     break;
                 case "date":
                 case "datetime":
